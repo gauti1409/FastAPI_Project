e7ca6a32eab1e88a75a8fb9f1926fe997292f6de
Dhruv_21 <dhruv.gautam1409@gmail.com>
2024-01-06 15:48:44 +0530

    updated files for relative import

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 # from main_files import database, models
 from .main_files import database, models
 from .routers import post, users, post_orm, users_orm, auth, vote
-import config
+from . import config
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 

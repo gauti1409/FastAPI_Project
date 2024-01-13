d5d670b4f5979c5225ea6b1a23db4776d4c08f76
Dhruv_21 <dhruv.gautam1409@gmail.com>
2024-01-13 18:27:13 +0530

    updated_files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+# import config
 # from main_files import database, models
 # from routers import post, users, post_orm, users_orm, auth, vote
-# import config
 from .main_files import database, models
 from .routers import post, users, post_orm, users_orm, auth, vote
 from . import config
